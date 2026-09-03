f079cd642e345af9369f613daaf7cfd09435c8d1
Mostafa <builtbymostafak@gmail.com>
2026-09-03 23:52:48 +0300

    Update app.php

--- src/views/layout/app.php
+++ src/views/layout/app.php
@@ -8,7 +8,7 @@ $path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Client Hub</title>
+<title>AA - Client Hub</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Lora:wght@300;400;500&display=swap">
